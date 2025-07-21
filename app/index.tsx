5937a7d97d7a055d78e04f661093d484c12160d2
Edersondacosta <34005488+Edersondacosta@users.noreply.github.com>
2025-07-21 13:36:35 -0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
 
+
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+    <View className="flex-1 justify-center items-center bg-red-600">
+      <Text className="text-5xl text-dark-200 font-bold">Bem vindo!!!!!</Text>
     </View>
   );
 }
